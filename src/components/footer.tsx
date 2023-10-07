9fe817363eda16b3f52b3eb0eb0364817cd4225e
IhsenBouallegue <bouallegueihsen@gmail.com>
2023-10-07 17:59:37 +0200

    add footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,52 @@
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { Button, buttonVariants } from "./ui/button";
+import { Icons } from "./icons";
+
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="bg-primary text-primary-foreground p-6 flex items-center justify-between">
+      <Icons.logo />
+      <div className="flex">
+        <Link
+          href="/"
+          className={cn(
+            buttonVariants({ variant: "link" }),
+            "text-primary-foreground",
+          )}
+        >
+          Link
+        </Link>
+        <Link
+          href="/"
+          className={cn(
+            buttonVariants({ variant: "link" }),
+            "text-primary-foreground",
+          )}
+        >
+          Link
+        </Link>
+        <Link
+          href="/"
+          className={cn(
+            buttonVariants({ variant: "link" }),
+            "text-primary-foreground",
+          )}
+        >
+          Link
+        </Link>
+      </div>
+      <div>
+        <Button variant="ghost" size="icon">
+          <Icons.twitter />
+        </Button>
+        <Button variant="ghost" size="icon">
+          <Icons.settings />
+        </Button>
+        <Button variant="ghost" size="icon">
+          <Icons.pizza />
+        </Button>
+      </div>
+    </div>
+  );
 }
